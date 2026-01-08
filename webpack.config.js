887d53e07285b7591cc887634fbe4969cdcac324
iddj iddj <iddj27510@outlook.com>
2026-01-08 08:38:19 +0100

    Fix Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,75 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 async function getHttpsOptions() {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-  return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
+    const httpsOptions = await devCerts.getHttpsServerOptions();
+    return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
 }
 
 module.exports = async (env, options) => {
-  const config = {
-    devtool: "source-map",
-    entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      taskpane: ["./src/taskpane/taskpane.ts", "./src/taskpane/taskpane.html"],
-      commands: ["./src/commands/commands.ts", "./src/commands/commands.html"],
-    },
-    resolve: { extensions: [".ts", ".html", ".js"] },
-    module: {
-      rules: [
-        { test: /\.ts$/, use: "babel-loader", exclude: /node_modules/ },
-        { test: /\.html$/, use: "html-loader", exclude: /node_modules/ },
-        {
-          test: /\.(png|jpg|gif|ico)$/,
-          type: "asset/resource",
-          generator: { filename: "assets/[name][ext]" },
+    // Proveravamo da li je development mod (lokalno) ili production (Cloudflare)
+    const dev = options.mode === "development";
+
+    const config = {
+        devtool: "source-map",
+        entry: {
+            polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+            taskpane: ["./src/taskpane/taskpane.ts", "./src/taskpane/taskpane.html"],
+            commands: ["./src/commands/commands.ts", "./src/commands/commands.html"],
         },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./src/taskpane/taskpane.html",
-        chunks: ["polyfill", "taskpane"],
-      }),
-      new HtmlWebpackPlugin({
-        filename: "commands.html",
-        template: "./src/commands/commands.html",
-        chunks: ["polyfill", "commands"],
-      }),
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: "assets/*", to: "assets/[name][ext]" },
-          
-          // Ovo kopira i manifest.xml i manifest.prod.xml (ako postoji)
-          { from: "manifest*.xml", to: "[name][ext]" },
+        resolve: { extensions: [".ts", ".html", ".js"] },
+        module: {
+            rules: [
+                { test: /\.ts$/, use: "babel-loader", exclude: /node_modules/ },
+                { test: /\.html$/, use: "html-loader", exclude: /node_modules/ },
+                {
+                    test: /\.(png|jpg|gif|ico)$/,
+                    type: "asset/resource",
+                    generator: { filename: "assets/[name][ext]" },
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: "taskpane.html",
+                template: "./src/taskpane/taskpane.html",
+                chunks: ["polyfill", "taskpane"],
+            }),
+            new HtmlWebpackPlugin({
+                filename: "commands.html",
+                template: "./src/commands/commands.html",
+                chunks: ["polyfill", "commands"],
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: "assets/*", to: "assets/[name][ext]" },
 
-          { from: "src/static/support.html", to: "support.html", noErrorOnMissing: true },
-          { from: "src/static/privacy.html", to: "privacy.html", noErrorOnMissing: true },
-          
-          // OVO JE POPRAVLJENO:
-          { 
-            from: "src/static/_headers", 
-            to: "_headers", 
-            toType: "file", // <--- OVO REŠAVA TVOJ PROBLEM!
-            noErrorOnMissing: true 
-          },
+                    { from: "manifest*.xml", to: "[name][ext]" },
+
+                    { from: "src/static/support.html", to: "support.html", noErrorOnMissing: true },
+                    { from: "src/static/privacy.html", to: "privacy.html", noErrorOnMissing: true },
+
+                    {
+                        from: "src/static/_headers",
+                        to: "_headers",
+                        toType: "file",
+                        noErrorOnMissing: true
+                    },
+                ],
+            }),
         ],
-      }),
-    ],
-    devServer: {
-      headers: { "Access-Control-Allow-Origin": "*" },
-      server: { type: "https", options: await getHttpsOptions() },
-      port: 3000,
-      hot: false,
-      liveReload: true,
-      watchFiles: ["src/**/*"],
-    },
-  };
+        devServer: {
+            headers: { "Access-Control-Allow-Origin": "*" },
+            server: {
+                type: "https",
+                // KLJUČNO: Ako je 'dev', traži sertifikate. Ako nije (Cloudflare), ne traži ništa.
+                options: dev ? await getHttpsOptions() : {}
+            },
+            port: 3000,
+            hot: false,
+            liveReload: true,
+            watchFiles: ["src/**/*"],
+        },
+    };
 
-  return config;
+    return config;
 };
