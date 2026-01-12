738540c2307c0566c80b0feaf76cd66070840476
iddj iddj <iddj27510@outlook.com>
2026-01-12 22:47:43 +0100

    fix(webpack): add HtmlWebpackPlugin for commands.html
    - Commands.html now properly processed through webpack
    - Bundled scripts (polyfill, vendors, commands) injected automatically
    - Added ESLint override for webpack.config.js (CommonJS requires)
    - Fixes broken commands.html in production build
    
    Resolves: Critical issue #6 (commands.html webpack config missing)

--- webpack.config.js
+++ webpack.config.js
@@ -8,125 +8,133 @@ const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 async function getHttpsOptions() {
-  const httpsOptions = await devCerts.getHttpsServerOptions();
-  return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
+    const httpsOptions = await devCerts.getHttpsServerOptions();
+    return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
 }
 
 module.exports = async (env, options) => {
-  const dev = options.mode === "development";
+    const dev = options.mode === "development";
 
-  const config = {
-    devtool: dev ? "source-map" : false,
-    entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      taskpane: ["./src/taskpane/taskpane.ts"],
-      commands: ["./src/commands/commands.ts"],
-    },
-    resolve: { extensions: [".ts", ".html", ".js"] },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          use: "babel-loader",
-          exclude: /node_modules/,
+    const config = {
+        devtool: dev ? "source-map" : false,
+        entry: {
+            polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+            taskpane: ["./src/taskpane/taskpane.ts"],
+            commands: ["./src/commands/commands.ts"],
         },
-            {
-                test: /\.html$/,
-                use: "html-loader",
-                exclude: [
-                    /node_modules/,
-                    path.resolve(__dirname, "src/taskpane/taskpane.html"),
-                    path.resolve(__dirname, "src/commands/commands.html"),
+        resolve: { extensions: [".ts", ".html", ".js"] },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "babel-loader",
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.html$/,
+                    use: "html-loader",
+                    exclude: [
+                        /node_modules/,
+                        path.resolve(__dirname, "src/taskpane/taskpane.html"),
+                        path.resolve(__dirname, "src/commands/commands.html"),
+                    ],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [MiniCssExtractPlugin.loader, "css-loader"],
+                },
+                {
+                    test: /\.(png|jpg|gif|ico)$/,
+                    type: "asset/resource",
+                    generator: { filename: "assets/[name][ext]" },
+                },
+            ],
+        },
+        optimization: {
+            minimize: !dev,
+            minimizer: [
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            drop_console: !dev,
+                        },
+                        mangle: {
+                            toplevel: true,
+                        },
+                        output: {
+                            comments: false,
+                        },
+                    },
+                    extractComments: false,
+                }),
+            ],
+            splitChunks: {
+                chunks: "all",
+                cacheGroups: {
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "vendors",
+                        priority: 10,
+                        enforce: true,
+                    },
+                },
+            },
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            }),
+
+            new HtmlWebpackPlugin({
+                filename: "taskpane.html",
+                template: "./src/taskpane/taskpane.html",
+                chunks: ["polyfill", "taskpane"],
+                templateParameters: { appVersion: pkg.version },
+                minify: !dev
+                    ? {
+                        collapseWhitespace: true,
+                        removeComments: true,
+                        removeRedundantAttributes: true,
+                    }
+                    : false,
+            }),
+
+            new HtmlWebpackPlugin({
+                filename: "commands.html",
+                template: "./src/commands/commands.html",
+                chunks: ["polyfill", "commands"],
+                minify: !dev
+                    ? {
+                        collapseWhitespace: true,
+                        removeComments: true,
+                        removeRedundantAttributes: true,
+                    }
+                    : false,
+            }),
+
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: "assets/*", to: "assets/[name][ext]" },
+                    { from: "manifest*.xml", to: "[name][ext]" },
+                    { from: "src/static/index.html", to: "index.html", noErrorOnMissing: true },
+                    { from: "src/static/support.html", to: "support.html", noErrorOnMissing: true },
+                    { from: "src/static/privacy.html", to: "privacy.html", noErrorOnMissing: true },
+                    { from: "src/static/_headers", to: "_headers", toType: "file", noErrorOnMissing: true },
                 ],
-            },
-
-        // CSS bundling (taskpane.css se uvozi iz taskpane.ts)
-        {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-
-        {
-          test: /\.(png|jpg|gif|ico)$/,
-          type: "asset/resource",
-          generator: { filename: "assets/[name][ext]" },
-        },
-      ],
-    },
-    optimization: {
-      minimize: !dev,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: !dev,
-            },
-            mangle: {
-              toplevel: true,
-            },
-            output: {
-              comments: false,
-            },
-          },
-          extractComments: false,
-        }),
-      ],
-      splitChunks: {
-        chunks: "all",
-        cacheGroups: {
-          vendors: {
-            test: /[\\/]node_modules[\\/]/,
-            name: "vendors",
-            priority: 10,
-            enforce: true,
-          },
-        },
-      },
-    },
-    plugins: [
-      // Izvlači CSS u fajl (npr. taskpane.css)
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-      }),
-
-        new HtmlWebpackPlugin({
-            filename: "taskpane.html",
-            template: "./src/taskpane/taskpane.html",
-            chunks: ["polyfill", "taskpane"],
-            templateParameters: { appVersion: pkg.version },
-            minify: !dev
-                ? {
-                    collapseWhitespace: true,
-                    removeComments: true,
-                    removeRedundantAttributes: true,
-                }
-                : false,
-        }),
-      new CopyWebpackPlugin({
-        patterns: [
-          { from: "assets/*", to: "assets/[name][ext]" },
-          { from: "manifest*.xml", to: "[name][ext]" },
-
-          // STATIC PAGES
-          { from: "src/static/index.html", to: "index.html", noErrorOnMissing: true },
-          { from: "src/static/support.html", to: "support.html", noErrorOnMissing: true },
-          { from: "src/static/privacy.html", to: "privacy.html", noErrorOnMissing: true },
-          { from: "src/static/_headers", to: "_headers", toType: "file", noErrorOnMissing: true },
+            }),
         ],
-      }),
-    ],
-    devServer: {
-      headers: { "Access-Control-Allow-Origin": "*" },
-      server: {
-        type: "https",
-        options: dev ? await getHttpsOptions() : {},
-      },
-      port: 3000,
-      hot: false,
-      liveReload: true,
-      watchFiles: ["src/**/*"],
-    },
-  };
+        devServer: {
+            headers: { "Access-Control-Allow-Origin": "*" },
+            server: {
+                type: "https",
+                options: dev ? await getHttpsOptions() : {},
+            },
+            port: 3000,
+            hot: false,
+            liveReload: true,
+            watchFiles: ["src/**/*"],
+        },
+    };
 
-  return config;
+    return config;
 };
