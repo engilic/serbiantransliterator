ee138b2008efaf8eec3de10091b1784133a565a5
iddj iddj <iddj27510@outlook.com>
2026-01-19 22:32:53 +0100

    chore: enforce gitignore for rust artifacts

--- src/wasm-core/target/wasm32-unknown-unknown/release/serbian_transliterator_wasm.d
+++
@@ -1,1 +0,0 @@
-D:\SerbianTransliteratorWeb\SerbianTransliterator\src\wasm-core\target\wasm32-unknown-unknown\release\serbian_transliterator_wasm.wasm: D:\SerbianTransliteratorWeb\SerbianTransliterator\src\wasm-core\src\lib.rs
