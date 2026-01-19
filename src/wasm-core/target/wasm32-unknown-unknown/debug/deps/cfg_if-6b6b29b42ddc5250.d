ae718cce9d673495a2ebf4bce2c13c84c0076028
iddj iddj <iddj27510@outlook.com>
2026-01-19 23:31:16 +0100

    chore: update gitignore and fix smoke test

--- src/wasm-core/target/wasm32-unknown-unknown/debug/deps/cfg_if-6b6b29b42ddc5250.d
+++
@@ -1,7 +0,0 @@
-D:\SerbianTransliteratorWeb\SerbianTransliterator\src\wasm-core\target\wasm32-unknown-unknown\debug\deps\cfg_if-6b6b29b42ddc5250.d: C:\Users\iddj2\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-D:\SerbianTransliteratorWeb\SerbianTransliterator\src\wasm-core\target\wasm32-unknown-unknown\debug\deps\libcfg_if-6b6b29b42ddc5250.rlib: C:\Users\iddj2\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-D:\SerbianTransliteratorWeb\SerbianTransliterator\src\wasm-core\target\wasm32-unknown-unknown\debug\deps\libcfg_if-6b6b29b42ddc5250.rmeta: C:\Users\iddj2\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
-
-C:\Users\iddj2\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
