e77115479cc1819428b578eb22c0ef70de90f0c7
engilic <127631897+engilic@users.noreply.github.com>
2026-01-20 15:50:50 +0100

    chore: webpack refactor, wasm optimization and security fix (undici) (#202)
    Co-authored-by: iddj iddj <iddj27510@outlook.com>

---
+++ webpack.common.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-undef */
+const path = require("path");
+const pkg = require("./package.json");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = {
+    entry: {
+        polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
+        taskpane: ["./src/taskpane/taskpane.ts"],
+        commands: ["./src/commands/commands.ts"],
+    },
+    resolve: {
+        extensions: [".ts", ".html", ".js"],
+    },
+    experiments: {
+        asyncWebAssembly: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "babel-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                use: "html-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|jpg|gif|ico)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/[name][ext]",
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+        }),
+        new HtmlWebpackPlugin({
+            filename: "taskpane.html",
+            template: "./src/taskpane/taskpane.html",
+            chunks: ["polyfill", "taskpane"],
+            templateParameters: {
+                appVersion: pkg.version,
+            },
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+            },
+        }),
+        new HtmlWebpackPlugin({
+            filename: "commands.html",
+            template: "./src/commands/commands.html",
+            chunks: ["polyfill", "commands"],
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+            },
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "assets/*", to: "assets/[name][ext]" },
+                { from: "manifest*.xml", to: "[name][ext]" },
+                { from: "src/static/index.html", to: "index.html", noErrorOnMissing: true },
+                { from: "src/static/support.html", to: "support.html", noErrorOnMissing: true },
+                { from: "src/static/privacy.html", to: "privacy.html", noErrorOnMissing: true },
+                { from: "src/static/_headers", to: "_headers", toType: "file", noErrorOnMissing: true },
+            ],
+        }),
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, "src/wasm-core"),
+            outDir: path.resolve(__dirname, "src/wasm-core/pkg"),
+        }),
+    ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,155 +1,29 @@
 /* eslint-disable no-undef */
-const path = require("path");
-const pkg = require("./package.json");
+const { merge } = require("webpack-merge");
 const devCerts = require("office-addin-dev-certs");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-async function getHttpsOptions() {
-    const httpsOptions = await devCerts.getHttpsServerOptions();
-    return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
-}
+const commonConfig = require("./webpack.common.js");
+const devConfig = require("./webpack.dev.js");
+const prodConfig = require("./webpack.prod.js");
 
 module.exports = async (env, options) => {
-    const dev = options.mode === "development";
+    const isDev = options.mode === "development";
+    const envConfig = isDev ? devConfig : prodConfig;
 
-    const config = {
-        devtool: dev ? "source-map" : false,
-        entry: {
-            polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-            taskpane: ["./src/taskpane/taskpane.ts"],
-            commands: ["./src/commands/commands.ts"],
-        },
-        resolve: { extensions: [".ts", ".html", ".js"] },
-
-        experiments: {
-            asyncWebAssembly: true,
-        },
-
-        module: {
-            rules: [
-                {
-                    test: /\.ts$/,
-                    use: "babel-loader",
-                    exclude: /node_modules/,
-                },
-                {
-                    test: /\.html$/,
-                    use: "html-loader",
-                    exclude: [
-                        /node_modules/,
-                        path.resolve(__dirname, "src/taskpane/taskpane.html"),
-                        path.resolve(__dirname, "src/commands/commands.html"),
-                    ],
-                },
-                {
-                    test: /\.css$/i,
-                    use: [MiniCssExtractPlugin.loader, "css-loader"],
-                },
-                {
-                    test: /\.(png|jpg|gif|ico)$/,
-                    type: "asset/resource",
-                    generator: { filename: "assets/[name][ext]" },
-                },
-            ],
-        },
-        optimization: {
-            minimize: !dev,
-            minimizer: [
-                new TerserPlugin({
-                    terserOptions: {
-                        compress: {
-                            drop_console: !dev,
-                        },
-                        mangle: {
-                            toplevel: true,
-                        },
-                        output: {
-                            comments: false,
-                        },
-                    },
-                    extractComments: false,
-                }),
-            ],
-            splitChunks: {
-                chunks: "all",
-                cacheGroups: {
-                    vendors: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: "vendors",
-                        priority: 10,
-                        enforce: true,
-                    },
-                },
-            },
-        },
-        // NEW: Suppress size warnings
-        performance: {
-            hints: false,
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: "[name].css",
-            }),
-
-            new HtmlWebpackPlugin({
-                filename: "taskpane.html",
-                template: "./src/taskpane/taskpane.html",
-                chunks: ["polyfill", "taskpane"],
-                templateParameters: { appVersion: pkg.version },
-                minify: !dev
-                    ? {
-                          collapseWhitespace: true,
-                          removeComments: true,
-                          removeRedundantAttributes: true,
-                      }
-                    : false,
-            }),
-
-            new HtmlWebpackPlugin({
-                filename: "commands.html",
-                template: "./src/commands/commands.html",
-                chunks: ["polyfill", "commands"],
-                minify: !dev
-                    ? {
-                          collapseWhitespace: true,
-                          removeComments: true,
-                          removeRedundantAttributes: true,
-                      }
-                    : false,
-            }),
-
-            new CopyWebpackPlugin({
-                patterns: [
-                    { from: "assets/*", to: "assets/[name][ext]" },
-                    { from: "manifest*.xml", to: "[name][ext]" },
-                    { from: "src/static/index.html", to: "index.html", noErrorOnMissing: true },
-                    { from: "src/static/support.html", to: "support.html", noErrorOnMissing: true },
-                    { from: "src/static/privacy.html", to: "privacy.html", noErrorOnMissing: true },
-                    { from: "src/static/_headers", to: "_headers", toType: "file", noErrorOnMissing: true },
-                ],
-            }),
-
-            new WasmPackPlugin({
-                crateDirectory: path.resolve(__dirname, "src/wasm-core"),
-                outDir: path.resolve(__dirname, "src/wasm-core/pkg"),
-            }),
-        ],
-        devServer: {
-            headers: { "Access-Control-Allow-Origin": "*" },
+    if (isDev) {
+        const httpsOptions = await devCerts.getHttpsServerOptions();
+        envConfig.devServer = {
+            ...envConfig.devServer,
             server: {
                 type: "https",
-                options: dev ? await getHttpsOptions() : {},
+                options: {
+                    ca: httpsOptions.ca,
+                    key: httpsOptions.key,
+                    cert: httpsOptions.cert,
+                },
             },
-            port: 3000,
-            hot: false,
-            liveReload: true,
-            watchFiles: ["src/**/*"],
-        },
-    };
+        };
+    }
 
-    return config;
+    return merge(commonConfig, envConfig);
 };
